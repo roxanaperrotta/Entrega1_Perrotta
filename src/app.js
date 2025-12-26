a68844712e3f401f7cef352c3c6f65a6f1a1ef6d
roxanaperrotta <136701143+roxanaperrotta@users.noreply.github.com>
2025-12-26 10:37:37 -0300

    Entrega1

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 import express from 'express';
-import mocksRouter from './routes/mocks.router.js'
+import mongoose from 'mongoose';
+import mocksRouter from './routes/mocks.router.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app= express();
+const MONGO_URI_ATLAS = process.env.MONGO_URI_ATLAS;
+
 
 app.use(express.json());
 
 app.use('/api/mocks', mocksRouter);
+
+const connectMongoDB = async () => {
+    try {
+      await mongoose.connect(MONGO_URI_ATLAS);
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('MongoDB connection error:', error.message);
+      process.exit(1);
+    }
+  };
+connectMongoDB();
 
 app.listen(8080, ()=>console.log('Servidor escuchando en el puerto 8080'))
--- src/routes/mocks.router.js
+++ src/routes/mocks.router.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { faker } from "@faker-js/faker";
+import { Pet } from "../models/pet.model.js";
+import { User } from "../models/user.model.js";
 
 const router = new Router();
 
 const generateUsers = (cant)=>{
     const users = Array.from({length: cant}, ()=>({
-        _id: faker.database.mongoObjectId(),
-        name: faker.person.firstName(),
+        _id: faker.database.mongodbObjectId(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
         age: faker.number.int({min:18, max:100}),
         email: faker.internet.email(),
         password: 'coder123'
@@ -16,7 +19,7 @@ const generateUsers = (cant)=>{
 
 const generatePets = (cant)=>{
     const pets = Array.from({length:cant}, ()=>({
-        _id: faker.database.mongoObjectId(),
+        _id: faker.database.mongodbObjectId(),
         name: faker.person.firstName(),
         age: faker.number.int({min:1, max:10}),
         species: faker.animal.type()
@@ -28,7 +31,7 @@ const generatePets = (cant)=>{
 router.get('/mockingpets/:cant', (req, res)=>{
     const {cant=50} = req.params;
     const pets = generatePets(cant)
-    res.send("pets")
+    res.send(pets)
 });
 
 router.get('/mockingusers/:cant', (req, res)=>{
@@ -37,13 +40,38 @@ router.get('/mockingusers/:cant', (req, res)=>{
     res.send(users);
 });
 
-router.post('/generatedata/:cantUsers/:cantPets', (req, res)=>{
-    const {cantUsers=50} = req.params;
-    const {cantPets = 50}= req.params;
-    const users = generateUsers(cantUsers);
-    const pets = generatePets(cantPets);
+router.post('/generatedata/:cantUsers/:cantPets', async (req, res)=>{
+    try {
+        const cantUsers = parseInt(req.params.cantUsers) || 50;
+        const cantPets = parseInt(req.params.cantPets) || 50;
+    
+        const users = generateUsers(cantUsers);
+        const pets = generatePets(cantPets);
+    
+        console.log(users)
+        console.log(pets)
 
-    res.send('data generada exitosamente')
-})
+        await User.insertMany(users);
+        await Pet.insertMany(pets);
+    
+        res.status(201).json({
+          status: 'success',
+          message: 'Data generada exitosamente',
+          usersInserted: users.length,
+          petsInserted: pets.length
+        });
+      } catch (error) {
+        res.status(500).json({
+          status: 'error',
+          message: error.message
+        });
+      }
+    });
+    
+    
+    
+    
+    
+    
 
 export default router;
